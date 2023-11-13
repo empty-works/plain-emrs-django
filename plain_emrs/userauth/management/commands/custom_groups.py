9a295b1240149f665e736fad076c14e0f83c6dcc
empty-works <pama.mark@gmail.com>
2023-11-13 20:34:40 +0100

    Updated permissions and groups for better granularity and avoiding excessive overlap.

--- plain_emrs/userauth/management/commands/custom_groups.py
+++ plain_emrs/userauth/management/commands/custom_groups.py
@@ -8,6 +8,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         # Create groups
+        patient_group, created = Group.objects.get_or_create(name='Patients')
         doctor_group, created = Group.objects.get_or_create(name='Doctors')
         clinical_staff_group, created = Group.objects.get_or_create(name='Clinical Staff')
         attending_physician_group, created = Group.objects.get_or_create(name='Attending Physicians')
@@ -16,11 +17,35 @@ class Command(BaseCommand):
         it_staff_group, created = Group.objects.get_or_create(name='IT Staff')
         system_admin_group, created = Group.objects.get_or_create(name='System Administrators')
 
+        # Permissions
+        view_patient_record = 'view_patient_record'
+        update_demographic_info = 'update_demographic_info'
+        update_clinical_data = 'update_clinical_data'
+        view_patient_medical_history = 'view_patient_medical_history'
+        update_medical_history = 'update_medical_history'
+        view_diagnoses = 'view_diagnoses'
+        update_diagnoses = 'update_diagnoses'
+        view_treatment_plans = 'view_treatment_plans'
+        update_treatment_plans = 'update_treatment_plans'
+        prescribe_medication = 'prescribe_medication'
+        can_order_tests = 'can_order_tests'
+        document_clinical_notes = 'document_clinical_notes'
+        can_contact_staff = 'can_contact_staff'
+
         # Assign permissions to groups
-        self.assign_permissions(doctor_group, ['view_patient_record', 'update_patient_record', 'view_patient_medical_history', 
-                                               'update_medical_history', 'view_diagnoses', 'update_diagnoses', 
-                                               'view_treatment_plans', 'update_treatment_plans', 'prescribe_medication', 
-                                               'can_order_tests', 'document_clinical_notes'])
+
+        # Common groups
+        self.assign_permissions(patient_group, [view_patient_record, update_demographic_info])
+
+        self.assign_permissions(clinical_staff_group, [view_patient_record, update_demographic_info, update_clinical_data, view_patient_medical_history, 
+                                                       update_medical_history, can_contact_staff])
+
+        # Specialized groups
+        self.assign_permissions(doctor_group, [view_diagnoses, update_diagnoses, view_treatment_plans, update_treatment_plans, prescribe_medication, 
+                                               can_order_tests, document_clinical_notes])
+
+
+
 
         self.stdout.write(self.style.SUCCESS('Groups and permissions created successfully'))
 
