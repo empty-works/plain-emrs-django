fbe28b765fde9d9b820a0a716ef4dfb6d40c7511
empty-works <pama.mark@gmail.com>
2023-11-15 20:10:46 +0100

    Completed groups and their permissions for now.

--- plain_emrs/userauth/management/commands/custom_groups.py
+++ plain_emrs/userauth/management/commands/custom_groups.py
@@ -17,34 +17,32 @@ class Command(BaseCommand):
         it_staff_group, created = Group.objects.get_or_create(name='IT Staff')
         system_admin_group, created = Group.objects.get_or_create(name='System Administrators')
 
-        # Permissions
+        # Shared permissions
         view_patient_record = 'view_patient_record'
         update_demographic_info = 'update_demographic_info'
-        update_clinical_data = 'update_clinical_data'
-        view_patient_medical_history = 'view_patient_medical_history'
-        update_medical_history = 'update_medical_history'
-        view_diagnoses = 'view_diagnoses'
-        update_diagnoses = 'update_diagnoses'
-        view_treatment_plans = 'view_treatment_plans'
-        update_treatment_plans = 'update_treatment_plans'
-        prescribe_medication = 'prescribe_medication'
-        can_order_tests = 'can_order_tests'
-        document_clinical_notes = 'document_clinical_notes'
-        can_contact_staff = 'can_contact_staff'
 
         # Assign permissions to groups
 
         # Common groups
         self.assign_permissions(patient_group, [view_patient_record, update_demographic_info])
 
-        self.assign_permissions(clinical_staff_group, [view_patient_record, update_demographic_info, update_clinical_data, view_patient_medical_history, 
-                                                       update_medical_history, can_contact_staff])
+        self.assign_permissions(clinical_staff_group, [view_patient_record, update_demographic_info, 'update_clinical_data', 'view_patient_medical_history', 
+                                                       'update_medical_history', 'can_contact_staff'])
+
+        self.assign_permissions(it_staff_group, ['access_system_configuration', 'access_maintenance_tools', 'view_logs'])
+
+        # view_patient_record includes all the following 'view' permissions
+        self.assign_permissions(administrative_staff_group, ['view_demographic_info', update_demographic_info, 'register_patient', 'view_appointments', 'schedule_appointments'])
 
         # Specialized groups
-        self.assign_permissions(doctor_group, [view_diagnoses, update_diagnoses, view_treatment_plans, update_treatment_plans, prescribe_medication, 
-                                               can_order_tests, document_clinical_notes])
+        self.assign_permissions(doctor_group, ['view_diagnoses', 'update_diagnoses', 'view_treatment_plans', 'update_treatment_plans', 'prescribe_medication', 
+                                               'can_order_tests', 'document_clinical_notes'])
 
+        self.assign_permissions(attending_physician_group, ['care_plan_approval', 'orders_approval'])
 
+        self.assign_permissions(specialist_group, ['prescribe_specialized_medication', 'authorize_device_implantation'])
+
+        self.assign_permissions(system_admin_group, ['access_security_settings', 'apply_updates'])
 
 
         self.stdout.write(self.style.SUCCESS('Groups and permissions created successfully'))
